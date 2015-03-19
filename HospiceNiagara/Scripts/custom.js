4d01d8a2857cfdd9bdebf2cda3343d457635d4b4
lunchboxsuhi <morozoandrei@gmail.com>
2015-03-19 17:48:27 -0400

    Initial Create

---
+++ HospiceNiagara/Scripts/custom.js
@@ -0,0 +1,9 @@
+﻿jQuery(document).ready(function () {
+
+   $("#menu-toggle").click(function(e) {
+       e.preventDefault();
+       $("#wrapper").toggleClass("toggled");
+       $("#menu-toggle").children().toggleClass("glyphicon-chevron-left");
+       $("#menu-toggle").children().toggleClass("glyphicon-chevron-right");
+   });
+});
