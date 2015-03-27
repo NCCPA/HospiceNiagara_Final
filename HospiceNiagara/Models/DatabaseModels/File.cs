2e9fa4ea33b5fb77022b5bfb09ff6cf0fd0dc46d
gee19 <gee19haine@gmail.com>
2015-03-27 15:48:13 -0400

    add db models for resources

---
+++ HospiceNiagara/Models/DatabaseModels/File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models.DatabaseModels
+{
+    public class File
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public int FolderID { get; set; }
+
+        [Required]
+        public byte[] FileContent { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string MimeType { get; set; }
+
+        [Required(ErrorMessage = "You cannot leave the name of the file blank.")]
+        [StringLength(100, ErrorMessage = "The name of the file cannot be more than 100 characters.")]
+        public string FileName { get; set; }
+
+        [StringLength(100, ErrorMessage = "The file description cannot be more than 100 characters.")]
+        public string FileDescription { get; set; }
+    }
+}
---
+++ HospiceNiagara/Models/DatabaseModels/Folder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models.DatabaseModels
+{
+    public class Folder
+    {
+        public Folder()
+        {
+
+        }
+
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "You cannot leave the name of the file blank.")]
+        [StringLength(100, ErrorMessage = "The name of the file cannot be more than 100 characters.")]
+        public string FolderName { get; set; }
+
+        [StringLength(100, ErrorMessage = "The file description cannot be more than 100 characters.")]
+        public string FolderDescription { get; set; }
+    }
+}
