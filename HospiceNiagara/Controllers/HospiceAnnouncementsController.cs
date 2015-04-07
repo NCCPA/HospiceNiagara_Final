105d41ccd6007ad06ba14852053beb359cd00b5b
Ryan Morris <saveourmacs@gmail.com>
2015-04-07 14:38:41 -0400

    hospiceViews

--- HospiceNiagara/Controllers/HospiceAnnouncementsController.cs
+++ HospiceNiagara/Controllers/HospiceAnnouncementsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using HospiceNiagara.Models;
 using HospiceNiagara.Models.DatabaseModels;
+using PagedList;
 
 namespace HospiceNiagara.Controllers
 {
@@ -16,9 +17,60 @@ namespace HospiceNiagara.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: HospiceAnnouncements
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View(db.Announcements.ToList());
+            SelectListItem allOption = new SelectListItem() { Value = "0", Text = "All" };
+
+            //Set Sort Order
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            //Grab all Meetings
+            var announce = from m in db.Announcements
+                       select m;
+
+            //Filter
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                announce = announce.Where(m => m.Title.Contains(searchString));
+            }
+
+
+
+            //Switch to See what sorting we are going to do
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    announce = announce.OrderByDescending(m => m.Title);
+                    break;
+                case "Date":
+                    announce = announce.OrderBy(m => m.Date);
+                    break;
+                case "date_desc":
+                    announce = announce.OrderByDescending(m => m.Date);
+                    break;
+                default:
+                    announce = announce.OrderBy(m => m.Title);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            return View(announce.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: HospiceAnnouncements/Details/5
--- HospiceNiagara/Controllers/HospiceMeetingsController.cs
+++ HospiceNiagara/Controllers/HospiceMeetingsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using HospiceNiagara.Models;
 using HospiceNiagara.Models.DatabaseModels;
+using PagedList;
 
 namespace HospiceNiagara.Controllers
 {
@@ -16,9 +17,60 @@ namespace HospiceNiagara.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: HospiceMeetings
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View(db.Meetings.ToList());
+            SelectListItem allOption = new SelectListItem() { Value = "0", Text = "All" };
+
+            //Set Sort Order
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            //Grab all Meetings
+            var meet = from m in db.Meetings
+                       select m;
+
+            //Filter
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                meet = meet.Where(m => m.Name.Contains(searchString));
+            }
+
+          
+
+            //Switch to See what sorting we are going to do
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    meet = meet.OrderByDescending(m => m.Name);
+                    break;
+                case "Date":
+                    meet = meet.OrderBy(m => m.Date);
+                    break;
+                case "date_desc":
+                    meet = meet.OrderByDescending(m => m.Date);
+                    break;
+                default:
+                    meet = meet.OrderBy(m => m.Name);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            return View(meet.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: HospiceMeetings/Details/5
